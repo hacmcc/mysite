be805e9b7392bbc3720ccf848af5168670baaec5
cmcc <13477553858@139.com>
2018-06-28 11:53:44 +0800

    v1

--- polls/views.py
+++ polls/views.py
@@ -18,4 +18,4 @@ def hours_ahead(request, offset):
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
     html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    return HttpResponse(html).....
